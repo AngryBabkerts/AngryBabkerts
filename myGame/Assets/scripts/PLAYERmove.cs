df23b497c54dc4a86af64664079aee8b330c0db7
Alina <jangubas@gmail.com>
2019-08-07 21:57:28 +0300

    может из=за того что я не все файлы выбрала не загрузилось все

---
+++ myGame/Assets/scripts/PLAYERmove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PLAYERmove : MonoBehaviour
+{ // This is the refrence to Rigitbody component called rb
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForse = 500f;
+
+    // We marked this as "fixed" Update because we are using it to mess with physycs
+    void FixedUpdate()
+    {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);  
+
+        if (Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForse * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+ 
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForse * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+
+        }
+
+        if(rb.position.y <-1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
